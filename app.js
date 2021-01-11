12415c51fb7a154d5e25302a6a27987b43c11e4e
Grantjos12 <grantjoslyn@gmail.com>
2021-01-11 13:52:36 -0600

    toggle for navigation working

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("this is app.js");
+//
+// Js code for toggle animation on click!
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+
+///On lcik toggle to active
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
